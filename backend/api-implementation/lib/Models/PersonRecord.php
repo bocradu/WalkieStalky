a8211baf3bcb8a8be1e3cb7b4bfe2efdda66d141
Ubuntu <weingaertnere@walkiestalky.2jsvf10rgoqunowvkef4z4ryff.ax.internal.cloudapp.net>
2016-05-21 14:06:39 +0000

    - Added JSON-based implementation for ID creation

--- backend/api-implementation/lib/Models/PersonRecord.php
+++ backend/api-implementation/lib/Models/PersonRecord.php
@@ -8,8 +8,8 @@ namespace SwaggerServer\lib\Models;
  * PersonRecord
  */
 class PersonRecord {
-    /* @var string $publicId A public technical id we can use to connect to the user */
-    private $publicId;
+    /* @var string $personId A public technical id we can use to connect to the user */
+    private $personId;
     /* @var string $name The name to display on other people&#39;s phones */
     private $name;
     /* @var string $phonenumber The phone number of the user */
