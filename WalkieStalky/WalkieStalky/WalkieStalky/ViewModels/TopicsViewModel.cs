485a2eb8403f589671804cbfe0f3f8ff558a044a
Bogdan Cimpoesu <ioan-bogdan.cimpoesu@haufe-lexware.com>
2016-05-21 15:13:59 +0300

    added

--- WalkieStalky/WalkieStalky/WalkieStalky/ViewModels/TopicsViewModel.cs
+++ WalkieStalky/WalkieStalky/WalkieStalky/ViewModels/TopicsViewModel.cs
@@ -22,9 +22,9 @@ namespace WalkieStalky.ViewModels
         public TopicsViewModel()
         {
             Topics = new ObservableCollection<Topic>(); 
-            Topics.Add(new Topic { Name = "Tomato", Type = "Fruit", Image = "tomato.png" });
-            Topics.Add(new Topic { Name = "Romaine Lettuce", Type = "Vegetable", Image = "lettuce.png" });
-            Topics.Add(new Topic { Name = "Zucchini", Type = "Vegetable", Image = "zucchini.png" });
+            Topics.Add(new Topic { TopicName = "Tomato", Type = "Fruit", Image = "tomato.png" });
+            Topics.Add(new Topic { TopicName = "Romaine Lettuce", Type = "Vegetable", Image = "lettuce.png" });
+            Topics.Add(new Topic { TopicName = "Zucchini", Type = "Vegetable", Image = "zucchini.png" });
 
             AddTopicCommand = new AddTopicCommand();
 
@@ -35,7 +35,7 @@ namespace WalkieStalky.ViewModels
     public class Topic
     {
         public string Image { get;  set; }
-        public string Name { get;  set; }
+        public string TopicName { get;  set; }
         public string Type { get;  set; }
     }
 
