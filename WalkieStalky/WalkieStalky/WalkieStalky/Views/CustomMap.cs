0a30385525aa92256d658de26231ea73be942302
Boc Radu <radu.boc@haufe-lexware.com>
2016-05-22 02:47:04 +0300

    added orientation

---
+++ WalkieStalky/WalkieStalky/WalkieStalky/Views/CustomMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace WalkieStalky.Views
+{
+    public class CustomMap:Map
+    {
+        public CustomMap()
+        {
+            HasScrollEnabled = false;
+            HasZoomEnabled = false;
+            WidthRequest = 960;
+            HeightRequest = 100;
+            IsShowingUser = true;
+            MapType = MapType.Street;
+            VerticalOptions = LayoutOptions.FillAndExpand;
+        }
+
+        public event EventHandler SwipeDown;
+        public event EventHandler SwipeTop;
+        public event EventHandler SwipeLeft;
+        public event EventHandler SwipeRight;
+
+        public void OnSwipeDown()
+        {
+            if (SwipeDown != null)
+                SwipeDown(this, null);
+        }
+
+        public void OnSwipeTop()
+        {
+            if (SwipeTop != null)
+                SwipeTop(this, null);
+        }
+
+        public void OnSwipeLeft()
+        {
+            if (SwipeLeft != null)
+                SwipeLeft(this, null);
+        }
+
+        public void OnSwipeRight()
+        {
+            if (SwipeRight != null)
+                SwipeRight(this, null);
+        }
+    }
+}
